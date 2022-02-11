084629a563d06cba7a7fbc7abe789a0be7852918
DesktopPC <mpalacz00@gmail.com>
2022-02-11 22:07:38 +0100

    Zad5

--- src/Zad4/Main.java
+++ src/Zad4/Main.java
@@ -5,7 +5,15 @@ public class Main {
         KeiCar keiCar;
         try {
             keiCar = new KeiCar("Honda", "Civic", 1600, 150, 3800, 2200, 1500);
-        } catch (TooBigEngineException | TooMuchPowerException | TooWideException | TooHighException | TooLongException e) {
+        } catch (TooBigEngineException e) {
+            e.printStackTrace();
+        } catch (TooMuchPowerException e) {
+            e.printStackTrace();
+        } catch (TooWideException e) {
+            e.printStackTrace();
+        } catch (TooHighException e) {
+            e.printStackTrace();
+        } catch (TooLongException e) {
             e.printStackTrace();
         }
     }
